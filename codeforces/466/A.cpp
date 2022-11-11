1690d19e1617110b2c9e73df779c6033ac752bc8
Abu Sufian <abusufian202262@gmail.com>
2022-11-11 23:43:00 +0600

    Add solution for problem `A - Cheap Travel` Link: https://codeforces.com/contest/466/problem/A Tags: implementation, *1200 Ref: https://codeforces.com/contest/466/submission/180502255

--- codeforces/466/A.cpp
+++ codeforces/466/A.cpp
@@ -7,13 +7,13 @@ int main()
     cin>>n>>m>>x>>y; 
     
     if(n%m==0){
-        int t=(n/m)*y;
-        cout<<((t<n*x)?t:n*x);
+        int t=(n/m)*y+(n%m)*x;
+        (t<n*x)?cout<<t<<endl:cout<<n*x<<endl;
     }
     else{
         t1=(n/m)*y+(n%m)*x;
         t2=(n/m+1)*y;
-        cout<<((t1<t2)?t1:t2);
+        (t1<t2)?cout<<t1:cout<<t2;
     }
     return 0;
 }
