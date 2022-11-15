223ddfea0e3e01a9b27be24ce85553e5a1cb01d6
Abu Sufian <abusufian202262@gmail.com>
2022-11-15 10:25:00 +0600

    Add solution for problem `A - Team` Link: https://codeforces.com/contest/231/problem/A Tags: brute force, greedy, *800 Ref: https://codeforces.com/contest/231/submission/181038089

--- codeforces/231/A.cpp
+++ codeforces/231/A.cpp
@@ -3,13 +3,18 @@ using namespace std;
 
 int main()
 {
-    int n,a,b,c,s=0;
+    int n,one=0,solve=0;
     cin>>n;
-    while(cin>>a>>b>>c){
-        s+=a+b+c>=2;
+    int a[n][3];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<3;j++){
+            cin>>a[i][j];
+            if(a[i][j]==1)one++;
+        }
+        if(one>=2)solve++;
+        one=0;
     }
-    cout<<s;
-    
+    cout<<solve;
 
     return 0;
 }
