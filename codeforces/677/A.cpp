58cefed765f127abdc27eb9e0e3755bfc2d1e6ec
Abu Sufian <abusufian202262@gmail.com>
2022-11-15 09:29:00 +0600

    Add solution for problem `A - Vanya and Fence` Link: https://codeforces.com/contest/677/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/677/submission/181035343

---
+++ codeforces/677/A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,h,w=0;
+    cin>>n>>h;
+    
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        (a[i]<=h)?w++:w+=2;
+    }
+    cout<<w;
+
+    return 0;
+}
