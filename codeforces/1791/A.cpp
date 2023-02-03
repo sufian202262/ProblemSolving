b80c3490e2b444664ca56a02d843360f8b008961
Abu Sufian <abusufian202262@gmail.com>
2023-02-03 21:20:00 +0600

    Add solution for problem `A - Codeforces Checking` Link: https://codeforces.com/contest/1791/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/1791/submission/191935935

---
+++ codeforces/1791/A.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;cin>>t;
+    while(t--){
+        char c;cin>>c;
+        if(c=='c')cout<<"YES\n";
+        else if(c=='o')cout<<"YES\n";
+        else if(c=='d')cout<<"YES\n";
+        else if(c=='e')cout<<"YES\n";
+        else if(c=='f')cout<<"YES\n";
+        else if(c=='r')cout<<"YES\n";
+        else if(c=='s')cout<<"YES\n";
+        else cout<<"NO\n";
+    }
+
+    return 0;
+}
