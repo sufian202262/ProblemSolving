887deb0bde63188025dec9ff3147b7ecc90892a5
Abu Sufian <abusufian202262@gmail.com>
2023-04-17 12:37:00 +0600

    Add solution for problem `A - Night at the Museum` Link: https://codeforces.com/contest/731/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/731/submission/202447036

--- codeforces/731/A.cpp
+++ codeforces/731/A.cpp
@@ -6,13 +6,28 @@ int main()
     string s;
     cin>>s;
     
-    char cur='a';
-    int sum=0;
-    for(auto c: s){
-        sum+=min(abs(cur-c),26-abs(cur-c));
-        cur=c;
+    int sum=0,k=0,dif1,dif2;
+    string str="abcdefghijklmnopqrstuvwxyz";
+    
+    for(int i=0;i<s.size();i++){
+        for(int j=0;j<str.size();j++){
+            if(s[i]==str[j]){
+                if(s[i]<str[k]){
+                    dif1=(str[k]-s[i]);
+                    if(str[k]=='a')dif2=(str[str.size()-1]-str[k])+1;
+                    else dif2=(str[str.size()-1]-str[k])+(s[i]-str[0])+1;
+                    (dif1<dif2)?sum+=dif1:sum+=dif2;
+                }
+                else if(s[i]>str[k]){
+                    dif1=(s[i]-str[k]);
+                    if(str[k]=='a')dif2=(str[str.size()-1]-s[i])+1;
+                    else dif2=(str[str.size()-1]-s[i])+(str[k]-str[0])+1;
+                    (dif1<dif2)?sum+=dif1:sum+=dif2;
+                }
+                k=j;
+            }
+        }
     }
     cout<<sum;
-    
     return 0;
 }
