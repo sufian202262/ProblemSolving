299efb19f4e2424b80f9aa4ba9897f053c150ddb
Abu Sufian <abusufian202262@gmail.com>
2023-04-24 22:16:00 +0600

    Add solution for problem `A - Team Olympiad` Link: https://codeforces.com/contest/490/problem/A Tags: greedy, implementation, sortings, *800 Ref: https://codeforces.com/contest/490/submission/203328392

--- codeforces/490/A.cpp
+++ codeforces/490/A.cpp
@@ -1,22 +1,49 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    int n,x;
-    cin>>n;
-    
-    vector<int> v[4];
+    int n;cin>>n;
+    int arr[n];
     
     for(int i=0;i<n;i++){
-        cin>>x;
-        v[x].push_back(i+1);
+        cin>>arr[i];
     }
-    int mini=min(v[1].size(),min(v[2].size(),v[3].size()));
-    cout<<mini<<endl;
     
-    for(int i=0;i<mini;i++){
-        cout<<v[1][i]<<" "<<v[2][i]<<" "<<v[3][i]<<"\n";
+    int one=0,two=0,three=0;
+    
+    for(int i=0;i<n;i++){
+        if(arr[i]==1)one++;
+        if(arr[i]==2)two++;
+        if(arr[i]==3)three++;
+    }
+    int mini=min(one,min(two,three));
+    
+    cout<<mini<<endl;
+    one=0;two=0;three=0;
+    
+    while(mini--){
+        for(int i=one;i<n;i++){
+            if(arr[i]==1){
+                cout<<i+1<<" ";
+                one=i+1;
+                break;
+            }
+        }
+        for(int i=two;i<n;i++){
+            if(arr[i]==2){
+                cout<<i+1<<" ";
+                two=i+1;
+                break;
+            }
+        }
+        for(int i=three;i<n;i++){
+            if(arr[i]==3){
+                cout<<i+1<<"\n";
+                three=i+1;
+                break;
+            }
+        }
     }
 
     return 0;
