4a3af5ddaac9cba47203eb632f29dcbf9568b09b
Abu Sufian <abusufian202262@gmail.com>
2023-05-02 22:31:00 +0600

    Add solution for problem `A - Fox And Snake` Link: https://codeforces.com/contest/510/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/510/submission/204275949

--- codeforces/510/A.cpp
+++ codeforces/510/A.cpp
@@ -3,16 +3,17 @@ using namespace std;
 
 int main()
 {
-    int row,col;
+    int row,col,l=0,r=0;
     cin>>row>>col;
     
     for(int i=1;i<=row;i++){
         for(int j=1;j<=col;j++){
             if(i%2!=0)cout<<"#";
-            else if(i%4==2 && j==col || i%4==0 && j==1)cout<<"#";
+            else if(i%2==0 && j==col && r==0 && l==0){cout<<"#";r=1;l=1;}
+            else if(i%2==0 && j==1 && r==1 && l==0){cout<<"#";r=0;l=1;}
             else cout<<".";
         }
-        
+        l=0;
         cout<<"\n";
     }
     return 0;
