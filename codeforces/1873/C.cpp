872c5f80ace9b5054399800243850f0a74a4dc58
Abu Sufian <abusufian202262@gmail.com>
2023-09-22 10:58:00 +0600

    Add solution for problem `C - Target Practice` Link: https://codeforces.com/contest/1873/problem/C Tags: implementation, math, *800 Ref: https://codeforces.com/contest/1873/submission/224558413

---
+++ codeforces/1873/C.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define cin(vec) for(auto& i : vec) cin >> i
+#define cout(vec) for(auto& i : vec) cout << i << " "; cout << "\n";
+#define cout_map(mp) for(auto& [f, s] : mp) cout << f << " : " << s << "\n"
+#define matrix(grid, n, m) vector < vector <ll> > grid(n, vector <ll> (m));
+#define ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define fixed(n) cout << fixed << setprecision(n)
+#define Num_of_Digits(n) ((int)log10(n)+1)
+#define getline(s) getline(cin >> ws, s)
+#define to_decimal(bin) stoll(bin, nullptr, 2)
+#define TC int t; cin >> t; while(t--)
+#define rall(s) s.rbegin(), s.rend()
+#define all(s) s.begin(), s.end()
+#define sz(x) int(x.size())
+#define pi pair <int, int>
+#define fi first
+#define se second
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+#define pb(x) push_back(x)
+#define ll long long
+
+#define out(x) {cout<< x <<"\n";};
+#define unq(x) x.erase(unique(x.begin(),x.end()),x.end())
+
+const char nl = '\n';
+
+int main()
+{   
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    TC{
+        vector<string> grid;
+        for(int i=0;i<10;i++){
+            string s;cin>>s;
+            grid.push_back(s);
+        }
+        int ans=0,mr,mc;
+        for(int i=0;i<10;i++){
+            for(int j=0;j<10;j++){
+                if(grid[i][j] == 'X' ){
+                    mr = min(i+1, 10-i);
+                    mc = min(j+1, 10-j);
+                    ans+=min(mr,mc);
+                }
+            }
+        }
+        out(ans);
+    }
+    
+  return 0;
+}
