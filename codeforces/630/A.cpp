c62321d82075ee4679e7d29d7d0c3c4840040dea
Abu Sufian <abusufian202262@gmail.com>
2024-01-08 16:58:00 +0600

    Add solution for problem `A - Again Twenty Five!` Link: https://codeforces.com/contest/630/problem/A Tags: number theory, *800 Ref: https://codeforces.com/contest/630/submission/240795688

---
+++ codeforces/630/A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+#define int long long
+
+#define endl '\n'
+#define out(x) {cout<< x <<"\n";};
+
+
+signed main(){   
+    int n;
+    cin>>n;
+    cout<<25<<endl;
+    return 0;
+}
